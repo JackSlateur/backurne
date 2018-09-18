b840f42bd9fe325efcb2f81d4ab7b21dbaa1570a
Alexandre Bruyelles <abruyelles@odiso.com>
2018-09-18 15:34:25 +0200

    stats.py: use multiprocessing to speed up the code
    Signed-off-by: Alexandre Bruyelles <abruyelles@odiso.com>

--- stats.py
+++ stats.py
@@ -2,20 +2,34 @@
 
 import json
 import humanize
+import multiprocessing
 from config import config
 from sh import rbd
 
+
 pool = config['backup_cluster']['pool']
-data = rbd('--format', 'json', '-p', pool, 'du')
+
+
+def du(image):
+	data = rbd('--format', 'json', '-p', pool, 'du', image)
+	data = data.stdout.decode('utf-8')
+	data = json.loads(data)['images']
+	return data
+
+
+data = rbd('--format', 'json', '-p', pool, 'ls')
 data = data.stdout.decode('utf-8')
-data = json.loads(data)['images']
+data = json.loads(data)
 
 result = {}
-for i in data:
-	try:
-		result[i['name']] += i['used_size']
-	except KeyError:
-		result[i['name']] = i['used_size']
+
+with multiprocessing.Pool(config['backup_worker']) as p:
+	for sizes in p.imap_unordered(du, data):
+		for i in sizes:
+			try:
+				result[i['name']] += i['used_size']
+			except KeyError:
+				result[i['name']] = i['used_size']
 
 result = [(k, result[k]) for k in sorted(result, key=result.get)]
 for key, value in result:
