3b5da56aa3c3fc3414b0467a617a19a297381c6d
Alexandre Bruyelles <jack@jack.fr.eu.org>
2023-03-06 19:01:31 +0100

    backurne: support python3.10+
    Signed-off-by: Alexandre Bruyelles <jack@jack.fr.eu.org>

--- src/backurne/config.py
+++ src/backurne/config.py
@@ -7,7 +7,7 @@ import imp
 def dict_merge(dct, merge_dct):
 	for k, v in merge_dct.items():
 		if k in dct and isinstance(dct[k], dict) \
-				and isinstance(merge_dct[k], collections.Mapping):
+				and isinstance(merge_dct[k], collections.abc.Mapping):
 			dict_merge(dct[k], merge_dct[k])
 		else:
 			dct[k] = merge_dct[k]
